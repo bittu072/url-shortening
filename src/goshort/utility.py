44d6d0f1011f64d6256e163c9cd7bf06526e182d
bittu072 <bittu.patel072@gmail.com>
2017-04-12 23:22:50 -0700

    added create_shorturl method to make shorturl unique

--- src/goshort/utility.py
+++ src/goshort/utility.py
@@ -7,3 +7,13 @@ def code_generator(size=10, chars=string.ascii_lowercase + string.ascii_uppercas
     #     new_code += random.choice(chars)
     # return new_code
     return ''.join(random.choice(chars) for i in range(size))
+
+def create_shorturl(instance, size=6):
+    new_url = code_generator(size=size)
+    klass = instance.__class__
+    # this is similar to importing ShortenURL class from models.py
+
+    qs_exists = klass.objects.filter(shorturl=new_url).exists()
+    if qs_exists:
+        return create_shorturl(size=size)
+    return new_code
