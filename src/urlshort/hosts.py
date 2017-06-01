f933f3dcd684d7d7490fbcb5a0998ee8a4ff249e
bittu072 <bittu.patel072@gmail.com>
2017-06-01 00:34:37 -0700

    added code to just allow blog.abcd.com and *.abcd.com url

--- src/urlshort/hosts.py
+++ src/urlshort/hosts.py
@@ -3,5 +3,9 @@ from django_hosts import patterns, host
 
 host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='www'),
+    # this is to redirect just "blog.abcd.com" url
+    host(r'blog', 'urlshort.hostsconf.urls', name='blog'),
+
+    # this is to redirect any prefix url "xx.abcd.com"
     host(r'(?!www).*', 'urlshort.hostsconf.urls', name='wildcard'),
 )
