ec9bd6f6bc53f99ef322b6ee32889618d9c03acf
bittu072 <bittu.patel072@gmail.com>
2017-06-01 00:29:14 -0700

    added comment to remove port 8000 in real implementation

--- src/urlshort/hostsconf/views.py
+++ src/urlshort/hostsconf/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
+# remove port 8000 in real implementation
 DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL", "http://www.trialshort.com:8000")
 
 def wildcard_redirect(request, path=None):
