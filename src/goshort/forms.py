4242e5dc9e739ac3ed1d989806dea68bf3b5e2f3
bittu072 <bittu.patel072@gmail.com>
2017-07-05 22:44:48 -0700

    added the clean method in URLSubmit class

--- src/goshort/forms.py
+++ src/goshort/forms.py
@@ -2,3 +2,8 @@ from django import forms
 
 class URLSubmit(forms.Form):
     url = forms.CharField(label='Submit URL')
+
+    def clean(self):
+        cleaned_data = super(URLSubmit, self).clean()
+        url = cleaned_data['url']
+        print (url)
