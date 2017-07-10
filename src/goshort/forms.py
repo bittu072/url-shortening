4eeadea694a0afaa76c405cfba6c0ec883e5f967
bittu072 <bittu.patel072@gmail.com>
2017-07-09 23:06:50 -0700

    instead of writing two method for cleaning data updated code to single method

--- src/goshort/forms.py
+++ src/goshort/forms.py
@@ -7,15 +7,21 @@ class URLSubmit(forms.Form):
 
     def clean(self):
         cleaned_data = super(URLSubmit, self).clean()
-        # url = cleaned_data['url']
-        # print (url)
-
-    def clean_url(self):
-        url = self.cleaned_data['url']
-        print (url)
+        url = cleaned_data.get('url')
         url_validator = URLValidator()
         try:
             url_validator(url)
         except:
             raise forms.ValidationError("Invalid URL")
+        return url
+        # print (url)
+
+    # def clean_url(self):
+    #     url = self.cleaned_data['url']
+    #     print (url)
+    #     url_validator = URLValidator()
+    #     try:
+    #         url_validator(url)
+    #     except:
+    #         raise forms.ValidationError("Invalid URL")
         return url
