9868e6ff5b44d9dde9a4d061923231df0c80968f
bittu072 <bittu.patel072@gmail.com>
2017-07-10 23:55:09 -0700

    created separate method and cleaned up the code

--- src/goshort/forms.py
+++ src/goshort/forms.py
@@ -1,19 +1,30 @@
 from django import forms
 
+from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
 
-class URLSubmit(forms.Form):
-    url = forms.CharField(label='Submit URL')
 
-    def clean(self):
-        cleaned_data = super(URLSubmit, self).clean()
-        url = cleaned_data.get('url')
-        url_validator = URLValidator()
-        try:
-            url_validator(url)
-        except:
-            raise forms.ValidationError("Invalid URL")
-        return url
+def validate_url(value):
+    url_validator = URLValidator()
+    try:
+        url_validator(value)
+    except:
+        raise forms.ValidationError("Invalid URL")
+    return value
+
+
+class URLSubmit(forms.Form):
+    url = forms.CharField(label='Submit URL', validators=[validate_url])
+
+    # def clean(self):
+    #     cleaned_data = super(URLSubmit, self).clean()
+    #     url = cleaned_data.get('url')
+    #     url_validator = URLValidator()
+    #     try:
+    #         url_validator(url)
+    #     except:
+    #         raise forms.ValidationError("Invalid URL")
+    #     return url
         # print (url)
 
     # def clean_url(self):
@@ -24,4 +35,4 @@ class URLSubmit(forms.Form):
     #         url_validator(url)
     #     except:
     #         raise forms.ValidationError("Invalid URL")
-        return url
+        # return url
